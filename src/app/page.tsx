93bc4115c24ab4644279efb60504de276076cc34
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-04-26 20:55:12 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className=" text-black text-4xl flex justify-center items-center min-h-screen">Ak is the best!</h1>
+  );
+}
